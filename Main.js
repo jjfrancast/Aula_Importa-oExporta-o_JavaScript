e8706dff65522e5f9527612d28072c259aa7fe4d
Perkles <jonastorres1993@gmail.com>
2022-10-19 23:16:19 -0300

    Commit Inicial

---
+++ Main.js
@@ -0,0 +1,20 @@
+
+const { gets, print } = require('./FuncoesAuxiliares');
+let maiorValor = 0;
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    if (numeroSorteado > maiorValorEncontrado) {
+        maiorValorEncontrado = numeroSorteado;
+    }
+}
+
+print(maiorValorEncontrado);
+
+
+
+
+
+
+
+
